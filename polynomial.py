f4c5e36ede2a0aab69b3a26d56f06da85c16b876
JOYCHEN <1196056610@qq.com>
2025-09-09 18:03:58 -0400

    first commit
    Added subtraction and division
    
    Added evaluate methods
    
    Added evaluate methods

--- polynomial.py
+++ polynomial.py
@@ -6,14 +6,10 @@ class X:
         return "X"
 
     def evaluate(self, x_value):
-        # TODO: Implement evaluation for variable X
-        # Should return an Int object with the given x_value
-        pass
+        return Int(x_value)
 
     def simplify(self):
-        # TODO (Optional Exercise): Implement simplification
-        # X cannot be simplified further, so return self
-        pass
+        return self
 
 
 class Int:
@@ -24,14 +20,10 @@ class Int:
         return str(self.i)
 
     def evaluate(self, x_value):
-        # TODO: Implement evaluation for integer constant
-        # Should return an Int object with the stored integer value
-        pass
+        return Int(self.i)
 
     def simplify(self):
-        # TODO (Optional Exercise): Implement simplification
-        # Integer constants cannot be simplified further, so return self
-        pass
+        return self
 
 
 class Add:
@@ -43,15 +35,45 @@ class Add:
         return repr(self.p1) + " + " + repr(self.p2)
 
     def evaluate(self, x_value):
-        # TODO: Implement evaluation for addition
-        # Should evaluate both operands and return their sum
-        pass
+        v1 = self.p1.evaluate(x_value)
+        v2 = self.p2.evaluate(x_value)
+        return Int(v1.i + v2.i)
 
     def simplify(self):
-        # TODO (Optional Exercise): Implement simplification
-        # Examples: X + 0 -> X, 0 + X -> X, 3 + 5 -> 8
-        # Hint: Simplify operands first, then apply simplification rules
-        pass
+        s1 = self.p1.simplify()
+        s2 = self.p2.simplify()
+
+        if isinstance(s1, Int) and s1.i == 0:
+            return s2
+        if isinstance(s2, Int) and s2.i == 0:
+            return s1
+
+        if isinstance(s1, Int) and isinstance(s2, Int):
+            return Int(s1.i + s2.i)
+        
+        if isinstance(s1, X) and isinstance(s2, X):
+            return Mul(Int(2), X())
+
+        if isinstance(s1, Mul) and isinstance(s2, Mul):
+            if isinstance(s1.p1, Int) and isinstance(s1.p2, X) and \
+               isinstance(s2.p1, Int) and isinstance(s2.p2, X):
+                return Mul(Int(s1.p1.i + s2.p1.i), X())
+            if isinstance(s1.p2, Int) and isinstance(s1.p1, X) and \
+               isinstance(s2.p2, Int) and isinstance(s2.p1, X):
+                return Mul(Int(s1.p2.i + s2.p2.i), X())
+
+        if isinstance(s1, X) and isinstance(s2, Mul):
+            if isinstance(s2.p1, Int) and isinstance(s2.p2, X):
+                return Mul(Int(s2.p1.i + 1), X())
+            if isinstance(s2.p2, Int) and isinstance(s2.p1, X):
+                return Mul(Int(s2.p2.i + 1), X())
+        if isinstance(s2, X) and isinstance(s1, Mul):
+            if isinstance(s1.p1, Int) and isinstance(s1.p2, X):
+                return Mul(Int(s1.p1.i + 1), X())
+            if isinstance(s1.p2, Int) and isinstance(s1.p1, X):
+                return Mul(Int(s1.p2.i + 1), X())
+
+        return Add(s1, s2)
 
 
 class Mul:
@@ -66,18 +88,35 @@ class Mul:
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+        if isinstance(self.p1, Sub):
+            if isinstance(self.p2, Sub):
+                return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+        if isinstance(self.p2, Sub):
+            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
     def evaluate(self, x_value):
-        # TODO: Implement evaluation for multiplication
-        # Should evaluate both operands and return their product
-        pass
+        v1 = self.p1.evaluate(x_value)
+        v2 = self.p2.evaluate(x_value)
+        return Int(v1.i * v2.i)
 
     def simplify(self):
-        # TODO (Optional Exercise): Implement simplification
-        # Examples: X * 0 -> 0, X * 1 -> X, 3 * 5 -> 15
-        # Hint: Simplify operands first, then apply simplification rules
-        pass
+        s1 = self.p1.simplify()
+        s2 = self.p2.simplify()
+
+        if (isinstance(s1, Int) and s1.i == 0) or (isinstance(s2, Int) and s2.i == 0):
+            return Int(0)
+
+        if isinstance(s1, Int) and s1.i == 1:
+            return s2
+        if isinstance(s2, Int) and s2.i == 1:
+            return s1
+
+        if isinstance(s1, Int) and isinstance(s2, Int):
+            return Int(s1.i * s2.i)
+
+        return Mul(s1, s2)
 
 
 class Sub:
@@ -86,21 +125,30 @@ class Sub:
         self.p2 = p2
 
     def __repr__(self):
-        # TODO: Implement string representation for subtraction
-        # Should handle parentheses similar to Mul class
-        # Hint: Look at how Mul class handles parentheses
-        pass
+        if isinstance(self.p1, Add):
+            if isinstance(self.p2, Add):
+                return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) - " + repr(self.p2)
+        if isinstance(self.p2, Add):
+            return repr(self.p1) + " - ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " - " + repr(self.p2)
 
     def evaluate(self, x_value):
-        # TODO: Implement evaluation for subtraction
-        # Should return the difference of the two operands
-        pass
+        v1 = self.p1.evaluate(x_value)
+        v2 = self.p2.evaluate(x_value)
+        return Int(v1.i - v2.i)
 
     def simplify(self):
-        # TODO (Optional Exercise): Implement simplification
-        # Examples: X - 0 -> X, 5 - 3 -> 2
-        # Hint: Simplify operands first, then apply simplification rules
-        pass
+        s1 = self.p1.simplify()
+        s2 = self.p2.simplify()
+
+        if isinstance(s2, Int) and s2.i == 0:
+            return s1
+
+        if isinstance(s1, Int) and isinstance(s2, Int):
+            return Int(s1.i - s2.i)
+
+        return Sub(s1, s2)
 
 
 class Div:
@@ -109,21 +157,40 @@ class Div:
         self.p2 = p2
 
     def __repr__(self):
-        # TODO: Implement string representation for division
-        # Should handle parentheses similar to Mul class
-        # Hint: Look at how Mul class handles parentheses
-        pass
+        if isinstance(self.p1, Sub):
+            if isinstance(self.p2, Sub):
+                return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if isinstance(self.p2, Sub):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        if isinstance(self.p1, Add):
+            if isinstance(self.p2, Add):
+                return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if isinstance(self.p2, Add):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " / " + repr(self.p2)
 
     def evaluate(self, x_value):
-        # TODO: Implement evaluation for division
-        # Should return the quotient of the two operands (use integer division //)
-        pass
+        v1 = self.p1.evaluate(x_value)
+        v2 = self.p2.evaluate(x_value)
+        if v2.i == 0:
+            raise ZeroDivisionError("Division by zero in Div.evaluate")
+        return Int(v1.i // v2.i)
 
     def simplify(self):
-        # TODO (Optional Exercise): Implement simplification
-        # Examples: X / 1 -> X, 6 / 2 -> 3
-        # Hint: Simplify operands first, then apply simplification rules
-        pass
+        s1 = self.p1.simplify()
+        s2 = self.p2.simplify()
+
+        if isinstance(s2, Int) and s2.i == 1:
+            return s1
+
+        if isinstance(s1, Int) and isinstance(s2, Int):
+            if s2.i == 0:
+                raise ZeroDivisionError("Division by zero in Div.simplify")
+            return Int(s1.i // s2.i)
+
+        return Div(s1, s2)
 
 
 # Original polynomial example
